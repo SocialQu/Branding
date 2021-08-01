a166874d5d4e6d00ad03879220a93e0f4c322151
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 16:45:03 -0500

    Last Week Interface.

---
+++ app/src/types/LastWeek.ts
@@ -0,0 +1,25 @@
+import { iKpis } from './LastDay'
+
+interface iMention {
+    user:string
+    link:string
+    niche:string
+    tweets:number
+    impressions:number
+}
+
+export interface iLastDay {
+    engagement:{
+        days:{ day:string, rate:number }[],
+        topics:{ topic:string, tweets:number, rate:number },
+        kpis:iKpis
+    },
+    activity:{
+        tweets:{ topic:string, kpis:iKpis }[],
+        replies:{ niche:string, kpis:iKpis }[]
+    },
+    reach:{
+        impressions:iMention[],
+        follows:{ audience:string, newFollowers:number }[]
+    }
+}
