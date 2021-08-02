7b8186914ef56ffb90121f45822ba0e9dc38179a
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 23:25:02 -0500

    Styled KPI

--- app/src/atoms/Kpi.tsx
+++ app/src/atoms/Kpi.tsx
@@ -1,10 +1,13 @@
 import { Property } from 'csstype'
 
 interface iKpi { label:string, value:number, color?:Property.Color ,isPercent?:boolean }
-export const Kpi = ({ label, value, color, isPercent }:iKpi) => <div className='level-item level-engagement'>
-    <p className='subtitle is-3' style={{color:'white', marginBottom: 20}}> 
+export const Kpi = ({ label, value, color, isPercent }:iKpi) => <div className='level-item level-kpi'>
+    <p style={{color:'white'}}> 
         <small style={{fontSize: '1rem'}}>{label}</small> <br/>
-        <span style={{color:color||'white'}}>{ value }</span>
-        <small>{isPercent && '%'}</small>
+        <span 
+            className="subtitle is-2" 
+            style={{color:color||'white', fontSize:!isPercent ? '2.5rem' : '2.25rem'}}
+        > { value }</span>
+        <small style={{fontSize:'1.5em'}}>{isPercent && '%'}</small>
     </p>
 </div>
