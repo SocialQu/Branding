420d74c40f6ea7dc2de1b6a71c5b3684d3c58d9c
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 20:03:11 -0500

    Import Bulma.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,8 @@
+import { LastDayKpis } from './organisms/KpiColumn'
+import 'bulma/css/bulma.css'
 import './App.css'
 
-export const App = () =><div className='App' />
+export const App = () =><div className='App'>
+    <LastDayKpis />
+</div>
+
