ffee33d822ecd69fcbabd416df64ac926abdb321
Santiago8888 <santiago.marti67@gmail.com>
2021-08-02 18:27:10 -0500

    Section Component.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,20 @@
 import { NavBar } from './layout/NavBar'
 import { Kpis } from './views/Kpis'
+import { ReactNode } from 'react'
 
 import 'bulma/css/bulma.css'
 import './App.css'
 
 
+interface iSection { children:ReactNode }
+const Section = ({ children }:iSection) => <div className="section" style={{padding:'2rem' }}>
+    <div className='container'> { children } </div>
+</div>
+
+
 export const App = () => <>
     <NavBar click={() => {}}/>
-    <div className="section" style={{padding:'2rem' }}>
-        <div className='container'>
-            <Kpis />
-        </div>
-    </div>
+    <Section>
+        <Kpis />
+    </Section>
 </>
