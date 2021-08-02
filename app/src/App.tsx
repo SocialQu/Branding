559a5e66dac211e294fca150bcb40d0d12f45751
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 22:21:18 -0500

    Add NavBar to App body.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,13 @@
+import { NavBar } from './layout/NavBar'
 import { Kpis } from './views/Kpis'
 
 import 'bulma/css/bulma.css'
 import './App.css'
 
 
-export const App = () => <div className='container'>
-    <Kpis />
-</div>
-
+export const App = () => <>
+    <NavBar click={() => {}}/>
+    <div className='container'>
+        <Kpis />
+    </div>
+</>
