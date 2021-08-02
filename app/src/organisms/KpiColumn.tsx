77bea1e1514f20acf1067ed8e06535ad4cdfbd63
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 20:20:04 -0500

    Add more data to Detail Ring.

--- app/src/organisms/KpiColumn.tsx
+++ app/src/organisms/KpiColumn.tsx
@@ -2,7 +2,13 @@ import { DetailRing } from '../charts/DetailRing'
 import { EngagementRate } from '../charts/EngagementRate'
 
 const data = [{Engagement:10, name:'Monday'}, {Engagement:5, name:'Tuesday'}]
-const detailData = [{ name:'Likes', value: 10 }]
+const detailData = [
+    { name:'Likes', value: 10 },
+    { name:'Retweets', value: 10 },
+    { name:'Replies', value: 10 },
+    { name:'Profile Visits', value: 10 }
+]
+
 export const LastDayKpis = () => <>
     <EngagementRate data={data}/>
     <DetailRing data={detailData} />
