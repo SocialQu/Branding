4880f021a607d81f2a4d023c1a2d9ce406f9790d
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 21:26:26 -0500

    Activity & Reach cards.

--- app/src/organisms/LastDay.tsx
+++ app/src/organisms/LastDay.tsx
@@ -1,5 +1,7 @@
-import { DetailRing } from '../charts/DetailRing'
 import { EngagementRate } from '../charts/EngagementRate'
+import { Activity, Reach } from '../molecules/KpiCards'
+import { DetailRing } from '../charts/DetailRing'
+
 
 const data = [{Engagement:10, name:'Monday'}, {Engagement:5, name:'Tuesday'}]
 const detailData = [
@@ -11,5 +13,7 @@ const detailData = [
 
 export const LastDay = () => <>
     <EngagementRate data={data}/>
+    <Activity tweets={10} replies={5}/>
+    <Reach impressions={1200} followers={12}/>
     <DetailRing data={detailData} />
 </>
