8fe8f9fb925bfc538fc85a1b782f8182a25fbdc8
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 22:20:37 -0500

    Import NavBar.

---
+++ app/src/layout/NavBar.tsx
@@ -0,0 +1,67 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+import { useState } from "react"
+
+export type TAB = 'HOME' | 'KPI' | 'GROWTH' | 'HEATMAP' | 'MONETIZATION' | 'COMPOSER'
+
+interface iNavBar { click(tab:TAB):void }
+export const NavBar = ({ click }: iNavBar) => {
+    const [ isActive, setActive ] = useState(false)
+    const clickTab = (tab: TAB) => {
+        isActive ? setActive(false) : setActive(isActive)
+        return click(tab)
+    }
+
+    return <nav className="navbar is-black" role="navigation" aria-label="main navigation">
+        <div className="container">
+            <div className="navbar-brand">
+                <a className="navbar-item" onClick={() => click('HOME')}>
+                    <img src="SocialQ.png" style={{ height:36, maxHeight: 'none' }} alt={'SocialQ logo'}/>
+                    <p className="navbar-item" style={{ fontSize: '2em', color:'white' }} > SocialQ </p>
+                </a>
+
+                <a 
+                    role="button" 
+                    className={`navbar-burger ${isActive ? 'is-active': ''}`}
+                    aria-label="menu" 
+                    aria-expanded="false" 
+                    data-target="navbarBasicExample"
+                    onClick={()=> setActive(!isActive)}
+                >
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+
+            <div className={`navbar-menu ${isActive ? 'is-active navbar-menu-active': ''}`} style={{ maxWidth:1200, marginRight:'auto' }}>
+                <div className={`navbar-start ${isActive ? 'is-active navbar-start-active': ''}`}>
+                    <a 
+                        className={`navbar-item ${isActive ? 'navbar-item-active': ''}`} 
+                        onClick={() => clickTab('KPI')}
+                    > KPIs </a>
+
+                    <a 
+                        className={`navbar-item ${isActive ? 'navbar-item-active': ''}`} 
+                        onClick={() => clickTab('GROWTH')}
+                    > Growth </a>
+
+                    <a 
+                        className={`navbar-item ${isActive ? 'navbar-item-active': ''}`}
+                        onClick={() => clickTab('MONETIZATION')}
+                    > Monetization </a>
+
+                    <a 
+                        className={`navbar-item ${isActive ? 'navbar-item-active': ''}`} 
+                        onClick={() => clickTab('COMPOSER')}
+                    > Composer </a>
+
+                    <a 
+                        className={`navbar-item ${isActive ? 'navbar-item-active': ''}`} 
+                        onClick={() => clickTab('HEATMAP')}
+                    > Heat Maps </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+}
