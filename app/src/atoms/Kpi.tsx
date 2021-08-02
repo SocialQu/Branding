1d450e744531c5fca1f8dcf89332b0144d2bed89
Santiago8888 <santiago.marti67@gmail.com>
2021-08-01 21:25:56 -0500

    Add color & percent props to KPI.

--- app/src/atoms/Kpi.tsx
+++ app/src/atoms/Kpi.tsx
@@ -1,7 +1,10 @@
-interface iKpi { label:string, value:number }
-export const Kpi = ({ label, value }:iKpi) => <div className='level-item level-engagement'>
-    <p className='subtitle is-3' style={{color: 'white', marginBottom: 20}}> 
+import { Property } from 'csstype'
+
+interface iKpi { label:string, value:number, color?:Property.Color ,isPercent?:boolean }
+export const Kpi = ({ label, value, color, isPercent }:iKpi) => <div className='level-item level-engagement'>
+    <p className='subtitle is-3' style={{color:'white', marginBottom: 20}}> 
         <small style={{fontSize: '1rem'}}>{label}</small> <br/>
-        <>{ value }<small>%</small></>
+        <span style={{color:color||'white'}}>{ value }</span>
+        <small>{isPercent && '%'}</small>
     </p>
 </div>
