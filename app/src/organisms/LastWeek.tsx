19988df0accac5d79f9bc7b5678d1dd0fd5dd071
Santiago8888 <santiago.marti67@gmail.com>
2021-08-04 01:15:44 -0500

    Engagement rate computation on Last Week.

--- app/src/organisms/LastWeek.tsx
+++ app/src/organisms/LastWeek.tsx
@@ -6,15 +6,22 @@ import { iLastWeek } from '../types'
 
 export const LastWeek = ({data}: {data:iLastWeek}) => <div className='column' style={{textAlign:'center'}} >
     <p className='subtitle is-4' style={{color: 'white'}}> Last Week KPIs </p>
-    <EngagementRate data={data.engagement.days.map(({ day, ...d }) => ({...d, name:day }))}/>
+    <EngagementRate 
+        data={
+            data.engagement.map(({ day, impressions, engagements }) => ({
+                name:day,
+                Engagement: Math.round(engagements*1000/impressions)/10
+            }))
+        }
+    />
     <Activity tweets={data.activity.tweets.length} replies={data.activity.replies.length}/>
-    <Reach impressions={data.reach.impressions} followers={data.reach.follows.length}/>
+    <Reach impressions={data.reach.impressions} followers={data.reach.follows}/>
     <DetailRing 
         data={[
-            { name:'Likes', value: data.engagement.kpis.likes},
-            { name:'Retweets', value: data.engagement.kpis.retweets},
-            { name:'Replies', value: data.engagement.kpis.replies},
-            { name:'Profile Visits', value: data.engagement.kpis.visits}
+            { name:'Likes', value: data.detail.kpis.likes},
+            { name:'Retweets', value: data.detail.kpis.retweets},
+            { name:'Replies', value: data.detail.kpis.replies},
+            { name:'Profile Visits', value: data.detail.kpis.visits}
         ]} 
     />
 </div>
