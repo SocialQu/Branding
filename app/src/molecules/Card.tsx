edc8c7fab94db35116386fa7728adde446a11f7d
Santiago8888 <santiago.marti67@gmail.com>
2021-08-04 14:14:00 -0500

    Extra props on card with icon and functionality.

--- app/src/molecules/Card.tsx
+++ app/src/molecules/Card.tsx
@@ -1,4 +1,9 @@
-import { CSSProperties } from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { CSSProperties, useState, ReactNode } from 'react'
+
+import '@creativebulma/bulma-collapsible/dist/css/bulma-collapsible.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
 
 const cardStyle:CSSProperties = {
     backgroundColor: 'rgb(48, 48, 48)',
@@ -15,15 +20,33 @@ const headerStyle:CSSProperties = {
     borderTopRightRadius: 12,
 }
 
-interface iCard {title: string, children?: any}
-export const Card = ({ title, children }:iCard) => <div className='card' style={cardStyle}>
-    <header className='card-header' style={headerStyle}>
-        <p className='card-header-title' style={{color:'white'}}> { title } </p>
-    </header>
+interface iCard {title: string, children?: ReactNode, extra?: ReactNode}
+export const Card = ({ title, children, extra }:iCard) => {
+    const [isHidden, setHidden] = useState(true)
 
-    <div className='card-content' style={{padding: '0.5rem 1rem'}}>
-        <div className='content'>
-            { children }
+    return <div className='card' style={cardStyle}>
+        <header className='card-header' style={headerStyle}>
+            <p className='card-header-title' style={{color:'white'}}> { title } </p>
+            {
+                extra && 
+                <a data-action="collapse" className="card-header-icon" onClick={() => setHidden(!isHidden)}>
+                    <span className="icon" style={{color:'white'}}>
+                        <i className={`fas fa-angle-${isHidden ? 'down' : 'up'}`} aria-hidden="true"/>
+                    </span>
+                </a>        
+            }
+        </header>
+
+        <div className='card-content' style={{padding: '0.5rem 1rem'}}>
+            <div className='content'>
+                { children }
+            </div>
+        </div>
+
+        <div id="collapsible-card" className="is-collapsible" style={{height:isHidden ? 0 : 'auto'}}>
+            <div className="card-content">
+                { extra }
+            </div>
         </div>
     </div>
-</div>
+}
