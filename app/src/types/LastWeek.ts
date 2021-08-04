bc68cd9ef28997ddf623890348d5732e010e7a81
Santiago8888 <santiago.marti67@gmail.com>
2021-08-04 01:02:57 -0500

    Separate followers from audience on iLastWeek.

--- app/src/types/LastWeek.ts
+++ app/src/types/LastWeek.ts
@@ -1,12 +1,18 @@
 import { iStats, iMention, iActivity, iDetail } from './LastDay'
 
-interface iDayEngagement extends iStats { day:string, followers:number }
-interface iAudience { audience:string, followers:number, following:number }
+export interface iDayEngagement extends iStats { day:string, followers:number }
+
+export interface iAudience { 
+    audience:string
+    followers:number
+    following:number
+    newFollowers:number 
+}
 
 
 export interface iLastWeek {
     engagement:iDayEngagement[],
     activity:iActivity,
-    reach:{ impressions:number, mentions:iMention[], follows:iAudience[] }
+    reach:{ impressions:number, mentions:iMention[], follows:number, audience:iAudience[] }
     detail:iDetail
 }
