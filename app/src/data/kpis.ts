3a035b9e0af878cb715bc6705d5b4a9ce5ee1ea0
Santiago8888 <santiago.marti67@gmail.com>
2021-08-04 01:03:14 -0500

    Adjust data to new interfaces.

--- app/src/data/kpis.ts
+++ app/src/data/kpis.ts
@@ -1,51 +1,39 @@
+import { iDayEngagement, iAudience } from '../types/LastWeek'
 import { iKpis, iLastDay, iLastWeek } from '../types'
 
 
 const kpis = { tweets:1, likes:3, replies:2, retweets:1, quotes:1, visits:2, clicks:2 }
+
+const tweets = [{ topic:'Topic', tweets:2, engagements:3, impressions:23 }]
+const replies = [{ niche:'Niche', tweets:2, engagements:3, impressions:23 }]
+const mentions = [{ user:'User', url:'url', impressions:1200, tweets:1, engagements:3 }]
 const follows = [{ url:'', user:'', followers:1000, following:100, audience:'Audience', tweets:10 }]
-const topics = [{ topic:'Topic', tweets:1, rate:1.5 }]
-
+const detail = { kpis, tweets:[{ text:'Text', kpis}] }
 const lastDay:iLastDay = {
-    engagement: {
-        tweets:[{ text:'Sample Tweet', engagement:1.5 }],
-        topics,
-        kpis
-    },
-
-    activity: {
-        tweets:[{ text:'Sample Tweet', topic:'Topic', kpis }],
-        replies:[{ text:'Sample Reply', niche:'Niche', kpis }]
-    },
-
-    reach: {
-        impressions:2200,
-        niches:[{ niche:'Niche', impressions:1200, tweets:1 }],
-        follows
-    }
+    engagement: [{ tweet:'Tweet', topic:'topic', tweets:1, impressions:1200, engagements:3 }],
+    activity: { tweets, replies },
+    reach: { impressions:2200, mentions, follows },
+    detail
 }
 
 
-const days = [
-    { engagement:0, day:'Monday' }, 
-    { engagement:0, day:'Tuesday' },
-    { engagement:0, day:'Wednesday' }, 
-    { engagement:0, day:'Thursday' },
-    { engagement:1.5, day:'Friday' }, 
-    { engagement:0, day:'Saturday' },
-    { engagement:0, day:'Sunday' }
+const days:iDayEngagement[] = [
+    { day:'Monday', engagements:0, impressions:3, tweets:3, followers:2 }, 
+    { day:'Tuesday', engagements:0, impressions:3, tweets:3, followers:2 },
+    { day:'Wednesday', engagements:0, impressions:3, tweets:3, followers:2 }, 
+    { day:'Thursday', engagements:0, impressions:3, tweets:3, followers:2 },
+    { day:'Friday', engagements:15, impressions:3, tweets:3, followers:2 }, 
+    { day:'Saturday', engagements:0, impressions:3, tweets:3, followers:2 },
+    { day:'Sunday', engagements:0, impressions:3, tweets:3, followers:2 }
 ]
 
+
+const audience:iAudience[] = [{ audience:'Audience', followers:4, following:4, newFollowers:3 }]
 const lastWeek:iLastWeek = {
-    engagement:{ days, topics, kpis },
-    activity:{
-        tweets:[{ topic:'Topic', kpis }],
-        replies:[{ niche:'Niche', kpis }]
-    },
-    reach:{
-        impressions:17400,
-        mentions:[{ user:'User', link:'', tweets:2, impressions:30, niche:'Niche' }],
-        follows:[{ audience:'Audience', newFollowers:4 }]
-    }
+    engagement:days,
+    activity:{ tweets, replies },
+    reach:{ impressions:17400, mentions, follows:4, audience },
+    detail
 }
 
 export const kpiData:iKpis = { lastDay, lastWeek }
