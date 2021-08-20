232efb4ab988b8a4f865b903af2d4e65de926c2e
Santiago8888 <santiago.marti67@gmail.com>
2021-08-20 06:14:25 -0500

    Classify Tweets by Topic.

---
+++ scripts/pipeline/3.Classify.ts
@@ -0,0 +1,35 @@
+// npx ts-node classify
+
+import tweets from '../data/training/embeddedTweets.json'
+import topics from '../data/training/topics.json'
+import { iEmbeddedTweet } from './2.Analysis'
+import { promises as fs } from 'fs'
+
+
+const findTopic = ({embeddings}:iEmbeddedTweet):string => {
+    const getSimilarity = (center:number[], embedding: number[]) => {
+        if (center.length !== embedding.length) return Infinity
+        const delta = center.reduce((d, i, idx) => d + Math.abs(i - embedding[idx]), 0)
+        return delta
+    }
+
+    topics.sort(({embeddings:a}, {embeddings:b}) => 
+        getSimilarity(embeddings, a) > getSimilarity(embeddings, b) ? 1 : -1
+    )
+
+    return topics[0].topic
+}
+
+
+export interface iLabeledTweet extends iEmbeddedTweet { topic:string }
+const classify = async():Promise<iLabeledTweet[]> => {
+    const nonReplies = tweets.filter(({ isReply }) => !isReply)
+    const labeledTweets = nonReplies.map(t => ({...t, topic:findTopic(t)}))
+
+    labeledTweets.map(({ text, topic}) => console.log(`${topic}: ${text}`))
+    await fs.writeFile('../data/training/labeledTweets.json', JSON.stringify(labeledTweets))
+    return labeledTweets
+}
+
+
+classify()
