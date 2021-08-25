d4063706ce8b5a302fdd10a771403d7537d21451
Santiago8888 <santiago.marti67@gmail.com>
2021-08-25 06:33:56 -0500

    Classify follower's bios by topic.

--- scripts/pipeline/3.Classify.ts
+++ scripts/pipeline/3.Classify.ts
@@ -1,12 +1,15 @@
 // npx ts-node 3.Classify
 
+import { iEmbeddedFollower, iEmbeddedTweet } from './2.Analysis'
+
+import followers from '../data/training/embeddedFollowers.json'
 import tweets from '../data/training/embeddedTweets.json'
 import topics from '../data/training/topics.json'
-import { iEmbeddedTweet } from './2.Analysis'
+
 import { promises as fs } from 'fs'
 
 
-const findTopic = ({embeddings}:iEmbeddedTweet):string => {
+const findTopic = ({embeddings}:{embeddings:number[]}):string => {
     const getSimilarity = (center:number[], embedding: number[]) => {
         if (center.length !== embedding.length) return Infinity
         const delta = center.reduce((d, i, idx) => d + Math.abs(i - embedding[idx]), 0)
@@ -22,7 +25,7 @@ const findTopic = ({embeddings}:iEmbeddedTweet):string => {
 
 
 export interface iLabeledTweet extends iEmbeddedTweet { topic:string }
-const classify = async():Promise<iLabeledTweet[]> => {
+const classifyTweets = async():Promise<iLabeledTweet[]> => {
     const nonReplies = tweets.filter(({ isReply }) => !isReply)
     const labeledTweets = nonReplies.map(t => ({...t, topic:findTopic(t)}))
 
@@ -31,5 +34,16 @@ const classify = async():Promise<iLabeledTweet[]> => {
     return labeledTweets
 }
 
+// classifyTweets()
 
-classify()
+
+export interface iLabeledFollower extends iEmbeddedFollower { topic:string }
+const classifyFollowers = async():Promise<iLabeledFollower[]> => {
+    const labeledFollowers = followers.map(t => ({...t, topic:findTopic(t)}))
+
+    labeledFollowers.map(({ bio, topic}) => console.log(`${topic}: ${bio}`))
+    await fs.writeFile('../data/training/labeledFollowers.json', JSON.stringify(labeledFollowers))
+    return labeledFollowers
+}
+
+classifyFollowers()
