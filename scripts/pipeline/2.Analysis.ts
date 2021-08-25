83374e9cff7346102efc8fd5d9c52f9d6a1677c6
Santiago8888 <santiago.marti67@gmail.com>
2021-08-25 06:33:30 -0500

    Embed Bios.

--- scripts/pipeline/2.Analysis.ts
+++ scripts/pipeline/2.Analysis.ts
@@ -1,8 +1,9 @@
 // npx ts-node 2.Analysis
 
 import * as use from '@tensorflow-models/universal-sentence-encoder'
+import followers from '../data/training/followers.json'
 import tweets from '../data/training/tweets.json'
-import { iTweet } from './1.Fetch'
+import { iFollower, iTweet } from './1.Fetch'
 
 import { promises as fs } from 'fs'
 import '@tensorflow/tfjs-node'
@@ -24,5 +25,22 @@ const embedTweets = async():Promise<iEmbeddedTweet[]> => {
     return embeddedTweets
 }
 
+// embedTweets().catch(console.log)
 
-embedTweets().catch(console.log)
+
+export interface iEmbeddedFollower extends iFollower { embeddings:number[] }
+
+const embedBios = async():Promise<iEmbeddedFollower[]> => {
+    const model = await use.load()
+    const bios = followers.map(({ bio }) => bio)
+
+    const tensors = await model.embed(bios)
+    const embeddings = tensors.arraySync()
+    
+    const embeddedFollowers = followers.map((t,i) => ({...t, embeddings:embeddings[i] }))
+
+    await fs.writeFile('../data/training/embeddedFollowers.json', JSON.stringify(embeddedFollowers))
+    return embeddedFollowers
+}
+
+embedBios().catch(console.log)
