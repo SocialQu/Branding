3c89db12a7600c73350cda729a488f1713140da6
Santiago8888 <santiago.marti67@gmail.com>
2021-08-26 04:02:56 -0500

    Definitive analysis interface.

--- scripts/types/analysis.ts
+++ scripts/types/analysis.ts
@@ -1,5 +1,4 @@
-import { iTweetBubble, iTweetTopic, iAudience } from './build'
-import { iUser } from '../pipeline/1.Fetch'
+import { iBaseData } from './build'
 
 
 interface kpi {
@@ -7,60 +6,12 @@ interface kpi {
     trend:number
 }
 
-interface correlation {
-    coefficient: number
-    ideal: number
-}
 
-interface suggestion {
-    topic: string
-    isNew: boolean
-    tweets: number
-    engagements: number
-}
-
-
-export interface iAnalysis {
-    user: iUser
+export interface iAnalysis extends iBaseData {
     kpis: {
         tweets:kpi
         engagements:kpi
         impressions:kpi
         newFollowers:kpi
-    }
-
-    bubbles:iTweetBubble[]
-    topics: iTweetTopic[]
-    audiences:iAudience[]
-
-    topTweets: {
-        id:number
-        topics:{
-            topic: string
-            color: string
-            percentage: number
-        }[]
-    }[]
-
-    links: {
-        tweets: number
-        engagements: number
-        impressions: number
-        clicks: number
-    }[]
-
-    correlations: {
-        link: correlation
-        frequency: correlation
-        lenght: correlation
-        emojis: correlation
-        position: correlation
-        time: correlation
-        weekday: correlation
-    }
-
-    suggestions: {
-        positive: suggestion
-        negative: suggestion
     }
 }
