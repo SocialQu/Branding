fcff149b7a580482fdbffe3f55c00b94107cda8e
Santiago8888 <santiago.marti67@gmail.com>
2021-08-26 05:28:03 -0500

    Compute tweets KPI average.

--- scripts/pipeline/7.Correlations.ts
+++ scripts/pipeline/7.Correlations.ts
@@ -1,12 +1,21 @@
 import { iKpi, iKpis, iAnalysis } from '../types/analysis'
 import data from '../data/training/buildData.json'
+import { iBuildData } from '../types/build'
+
 
 
 const getKpis = ():iKpis => {
     const baseKpi:iKpi = { average:0, trend:0 }
 
+    const { tweetDays }:iBuildData = data
+
+    const tweets:iKpi = {
+        average: tweetDays.reduce((d, { tweets }) => d += tweets.length, 0)/tweetDays.length,
+        trend: 0
+    }
+
     return {
-        tweets: baseKpi,
+        tweets,
         engagements: baseKpi,
         impressions: baseKpi,
         newFollowers: baseKpi
