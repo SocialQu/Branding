f3cca65b5d8c3ff933bb05e67e27c68cbdcf7111
Santiago8888 <santiago.marti67@gmail.com>
2021-08-26 06:00:29 -0500

    Compute every correlation.

--- scripts/pipeline/7.Correlations.ts
+++ scripts/pipeline/7.Correlations.ts
@@ -1,33 +1,30 @@
+// npx ts-node 7.Correlations
+
 import { iKpi, iKpis, iAnalysis } from '../types/analysis'
 import data from '../data/training/buildData.json'
 import { iBuildData } from '../types/build'
+import regression, { DataPoint } from 'regression'
 
 
+type kpi = 'tweets' | 'engagements' | 'impressions' | 'newFollowers'
+const getKPI = (metric:kpi):iKpi => {
+    const { tweetDays }:iBuildData = data
+    const average = tweetDays.reduce((d, i) => d += i[metric], 0)/tweetDays.length
+
+    const values = tweetDays.map((i, idx) => [idx, i[metric]] as DataPoint)
+    const { r2 } = regression.linear(values)
+
+    const kpi = { average, trend:r2 | 0 }
+    console.log('kpi:', kpi)
+
+    return kpi
+}
 
 const getKpis = ():iKpis => {
-    const baseKpi:iKpi = { average:0, trend:0 }
-
-    const { tweetDays }:iBuildData = data
-
-    const tweets:iKpi = {
-        average: tweetDays.reduce((d, { tweets }) => d += tweets, 0)/tweetDays.length,
-        trend: 0
-    }
-
-    const engagements:iKpi = {
-        average: tweetDays.reduce((d, { engagements }) => d += engagements, 0)/tweetDays.length,
-        trend: 0
-    }
-
-    const impressions:iKpi = {
-        average: tweetDays.reduce((d, { impressions }) => d += impressions, 0)/tweetDays.length,
-        trend: 0
-    }
-
-    const newFollowers:iKpi = {
-        average: tweetDays.reduce((d, { newFollowers }) => d += newFollowers, 0)/tweetDays.length,
-        trend: 0
-    }
+    const tweets:iKpi = getKPI('tweets')
+    const engagements:iKpi = getKPI('engagements')
+    const impressions:iKpi = getKPI('impressions')
+    const newFollowers:iKpi = getKPI('newFollowers')
 
     return { tweets, engagements, impressions, newFollowers }
 }
