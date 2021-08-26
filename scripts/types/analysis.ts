def6240131866eb1a83093e1a272bcde00892832
Santiago8888 <santiago.marti67@gmail.com>
2021-08-26 05:16:34 -0500

    KPIs interface.

--- scripts/types/analysis.ts
+++ scripts/types/analysis.ts
@@ -1,17 +1,17 @@
 import { iBaseData } from './build'
 
 
-interface kpi {
+export interface iKpi {
     average:number
     trend:number
 }
 
-
-export interface iAnalysis extends iBaseData {
-    kpis: {
-        tweets:kpi
-        engagements:kpi
-        impressions:kpi
-        newFollowers:kpi
-    }
+export interface iKpis {
+    tweets: iKpi
+    engagements: iKpi
+    impressions: iKpi
+    newFollowers: iKpi
 }
+
+
+export interface iAnalysis extends iBaseData { kpis: iKpis }
