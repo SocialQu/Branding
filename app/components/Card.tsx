877682e85f74e79005a12c4ae16283f0a4afc664
Santiago8888 <santiago.marti67@gmail.com>
2021-09-01 12:44:25 -0500

    Modularized card.

---
+++ app/components/Card.tsx
@@ -0,0 +1,31 @@
+import { CSSProperties, ReactNode } from 'react'
+
+
+const cardStyle:CSSProperties = {
+    backgroundColor: 'rgb(48, 48, 48)',
+    borderRadius: 12,
+    maxWidth:460,
+    margin: 'auto',
+    marginBottom: '1.5em',
+    border: '1px solid white'
+}
+
+const headerStyle:CSSProperties = {
+    backgroundColor: 'rgb(72, 72, 72)',
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+}
+
+
+interface iCard {title: string, children?: ReactNode }
+export const Card = ({ title, children }: iCard) => <div className='card' style={cardStyle}>
+    <header className='card-header' style={headerStyle}>
+        <p className='card-header-title' style={{color:'white'}}> { title } </p>
+    </header>
+
+    <div className='card-content' style={{padding: '0.5rem 1rem'}}>
+        <div className='content'>
+            { children }
+        </div>
+    </div>
+</div>
