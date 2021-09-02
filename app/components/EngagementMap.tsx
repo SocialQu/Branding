e79b1f4a7895292717fa08ab723cdfa88a4880e9
Santiago8888 <santiago.marti67@gmail.com>
2021-09-01 23:59:04 -0500

    Margins & axis on Scatter.

--- app/components/EngagementMap.tsx
+++ app/components/EngagementMap.tsx
@@ -12,15 +12,15 @@ const data = [
   { x: 110, y: 280, z: 200 },
 ]
 
-const margin = { top:20, right:20, bottom:20, left:20 }
+const margin = { top:20, right:0, bottom:0, left:0 }
 const BubbleChart = () => <ResponsiveContainer width="100%" height={400} >
     <ScatterChart width={400} margin={margin} >
-        <XAxis type="number" dataKey="x" />
-        <YAxis type="number" dataKey="y" />
+        <XAxis type="number" dataKey="x" tick={false} hide/>
+        <YAxis type="number" dataKey="y" tick={false} hide/>
         <ZAxis type="number" dataKey="z" range={[60, 400]}/>
 
         <Tooltip/>
-        <Scatter name="A school" data={data} fill="#8884d8" />
+        <Scatter data={data} fill="#8884d8" />
     </ScatterChart>
 </ResponsiveContainer>
 
