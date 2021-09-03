b40c69ad5e1e0c693d27eeaabf594007d2a4be46
Santiago8888 <santiago.marti67@gmail.com>
2021-09-02 23:49:28 -0500

    Engagement locations interface.

--- scripts/types/data.ts
+++ scripts/types/data.ts
@@ -3,15 +3,25 @@ import { iTweetBubble } from './build'
 export interface iKpis { tweets:number, engagements:number, impressions:number, followers:number }
 export interface iContent { topic:string, tweets:number, engagements:number, impressions:number }
 export interface iAudience { topic:string, followers:number }
+
+interface iLocation { x: number, y: number }
+export interface iEngagementLocations {
+    tweet: string, 
+    topic: string, 
+    location: iLocation, 
+    engagements: number
+}
+
 export interface iSuggestion {
     words: { text:string, value:number }[],
     topics: { topic:string, avgEngagement:number }[]
 }
 
+
 export interface iData {
     kpis: iKpis
     content: iContent
     audience: iAudience
-    engagementMap: iTweetBubble[]
+    engagementMap: iEngagementLocations[]
     suggestions: { positive:iSuggestion, negative:iSuggestion }
 }
