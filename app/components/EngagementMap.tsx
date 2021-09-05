657ee057ce19efc0ab0fca5bf8d879781433c7ae
Santiago8888 <santiago.marti67@gmail.com>
2021-09-05 02:27:07 -0500

    Locations data on Engagement Map.

--- app/components/EngagementMap.tsx
+++ app/components/EngagementMap.tsx
@@ -1,35 +1,32 @@
 import { ScatterChart, Scatter, XAxis, YAxis, ZAxis } from 'recharts'
 import { Tooltip, ResponsiveContainer } from 'recharts'
+import { iEngagementLocations } from '../types/data'
 import { Card } from './molecules/Card'
 
 
-const data = [
-  { x: 100, y: 200, z: 200 },
-  { x: 120, y: 100, z: 260 },
-  { x: 170, y: 300, z: 400 },
-  { x: 140, y: 250, z: 280 },
-  { x: 150, y: 400, z: 500 },
-  { x: 110, y: 280, z: 200 },
-]
-
 const margin = { top:20, right:0, bottom:0, left:0 }
-const BubbleChart = () => <ResponsiveContainer width="100%" height={400} >
+interface iBubbleData { x:number, y:number, z:number, tweet:string }
+interface iBubbleChart { data: iBubbleData[] }
+const BubbleChart = ({ data }:iBubbleChart) => <ResponsiveContainer width="100%" height={400} >
     <ScatterChart width={400} margin={margin} >
         <XAxis type="number" dataKey="x" tick={false} hide/>
         <YAxis type="number" dataKey="y" tick={false} hide/>
-        <ZAxis type="number" dataKey="z" range={[60, 400]}/>
+        <ZAxis type="number" dataKey="z" range={[10, 1000]}/>
 
-        <Tooltip/>
+        <Tooltip />
         <Scatter data={data} fill="#8884d8" />
     </ScatterChart>
 </ResponsiveContainer>
 
 
-
-export const EngagementMap = () => <div className={'columns'} style={{maxWidth:1200, margin:'auto'}}>
+interface iEngagementMap { data:iEngagementLocations[] }
+export const EngagementMap = ({ data }:iEngagementMap) => <div 
+    className={'columns'} 
+    style={{maxWidth:1200, margin:'auto'}}
+>
     <div className={'column'} style={{margin:'24px auto 48px'}}>
         <Card title={'Engagement Map'} cardStyle={{maxWidth:1060}}>
-            <BubbleChart />
+            <BubbleChart data={data.map(({ location:{ x, y }, engagements:z, tweet }) => ({ x, y, z, tweet }))} />
         </Card>
     </div>
 </div>
