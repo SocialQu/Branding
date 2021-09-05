d598b7e0dfdbe8d4bd6962b6aaed14bd3abcccfb
Santiago8888 <santiago.marti67@gmail.com>
2021-09-05 01:16:23 -0500

    Topic suggestions & word clouds.

--- scripts/pipeline/index.ts
+++ scripts/pipeline/index.ts
@@ -1,7 +1,10 @@
-import { iData, iEngagementLocations, iKpis, iContent, iAudience } from '../types/data'
+import { iData, iEngagementLocations, iKpis } from '../types/data'
+import { iContent, iAudience, iSuggestions } from '../types/data'
+
 import { iFetchedData, iMetrics } from '../types/fetch'
 import { analyzeData, iAnalysisData } from './analysis'
 import { fetchData } from './fetch'
+import { promises as fs } from 'fs'
 
 
 const getEngagements = (m: iMetrics) => m.likes + m.clicks + m.visits + m.replies + m.retweets
@@ -10,9 +13,9 @@ const getKpis = ({ user, tweets }:iFetchedData):iKpis => {
     const totalEngagements = engagements.reduce((d, i) => d+= i, 0)
 
     return {
-        tweets: tweets.length,
-        engagements: totalEngagements,
-        impressions: tweets.reduce((d, { metrics }) => d+= metrics.impressions, 0),
+        tweets: Math.round(tweets.length/30*10)/10,
+        engagements: Math.round(totalEngagements/30*10)/10,
+        impressions: Math.round(tweets.reduce((d, { metrics }) => d+= metrics.impressions, 0)/30*10)/10,
         followers: user.followers_count
     }
 }
@@ -23,26 +26,32 @@ const getLocations = ({ tweets }:iAnalysisData):iEngagementLocations[] => {
         tweet: text,
         location,
         engagements: getEngagements(metrics)
-    }))
+    })).sort(({ engagements:a }, { engagements:b }) => a > b ? -1 : 1)
 
     return locations
 }
 
-const getContent = ({ tweets }:iAnalysisData):iContent[] => {
+
+const getTopicsDictionary = ({ tweets }:iAnalysisData) => {
     const uniqueTopics = new Set(tweets.map(({ topic }) => topic))
     const topicsDict = [...uniqueTopics].map(topic => ({
         topic,
         tweets: tweets.filter(({ topic:t }) => topic === t)
     }))
 
+    return topicsDict
+}
+
+const getContent = (data:iAnalysisData):iContent[] => {
+    const topicsDict = getTopicsDictionary(data)
     const topics = topicsDict.map(({ topic, tweets }) => ({
         topic,
         tweets:tweets.length,
-        impressions: tweets.reduce((d, { metrics }) => d+= metrics.impressions, 0)/tweets.length,
-        engagements: tweets.reduce((d, { metrics }) => d+= getEngagements(metrics), 0)/tweets.length
-    }))
-    
-    return topics
+        impressions: tweets.reduce((d, { metrics }) => d+= metrics.impressions, 0),
+        engagements: tweets.reduce((d, { metrics }) => d+= getEngagements(metrics), 0)
+    })).sort(({ engagements: a }, { engagements: b }) => a > b ? -1 : 1)
+
+    return topics.filter((_, i) => i < 10)
 }
 
 const getNiches = ({ followers }:iAnalysisData):iAudience[] => {
@@ -52,12 +61,49 @@ const getNiches = ({ followers }:iAnalysisData):iAudience[] => {
         followers: followers.filter(({ niche:n }) => niche === n)
     }))
 
-    const niches = audienceDict.map(({ niche, followers }) => ({
-        niche,
-        followers: followers.length
+    const niches = audienceDict.map(({ niche, followers }) => ({ niche, followers: followers.length }))
+    const sortedNiches = niches.sort(({ followers:a }, { followers:b }) => a > b ? -1 : 1)
+    return sortedNiches.filter((_, i) => i < 10)
+}
+
+const getSuggestions = (data:iAnalysisData):iSuggestions => {
+    const topicsDict = getTopicsDictionary(data)
+
+    const topics = topicsDict.map(({ topic, tweets }) => ({
+        topic,
+        tweets:tweets.length,
+        words: tweets.reduce((d, { text }) => [...d, ...text.split(' ')], [] as string[]),
+        engagements: tweets.reduce((d, { metrics }) => d+= getEngagements(metrics), 0)/tweets.length
     }))
     
-    return niches
+    const engagementTopics = topics.map((t) => ({...t, avgEngagement: t.engagements/t.tweets }))
+    const sortedTopics = engagementTopics.sort(({ avgEngagement:a }, { avgEngagement:b }) => a > b ? -1 : 1)
+
+    const topTopics = sortedTopics.filter((_, i, l) => i < l.length/2).filter((_, i) => i < 5)
+    const bottomTopics = sortedTopics.filter((_, i, l) => l.length/2 < i).filter((_, i, l) => i + 6 > l.length)
+
+    const positiveTopics = topTopics.map(({ topic, avgEngagement }) => ({ topic, avgEngagement })) 
+    const negativeTopics = bottomTopics.map(({ topic, avgEngagement }) => ({ topic, avgEngagement })) 
+
+    const positiveList = topTopics.reduce((d, { words }) => [...d, ...words], [] as string[])
+    const negativeList = bottomTopics.reduce((d, { words }) => [...d, ...words], [] as string[])
+
+    const uniquePositive = [...new Set(positiveList)]
+    const uniqueNegative = [...new Set(negativeList)]
+
+    const positiveWords = uniquePositive.map(text => ({ text, value:positiveList.filter(w => w === text).length }))
+    const negativeWords = uniqueNegative.map(text => ({ text, value:negativeList.filter(w => w === text).length }))
+
+    const suggestions = {
+        positive: { topics:positiveTopics, words:positiveWords },
+        negative: { topics:negativeTopics, words:negativeWords }
+    }
+
+
+    console.log('Positive: ', suggestions.positive)
+    console.log('Negative: ', suggestions.negative)
+
+    return suggestions
 }
 
 
@@ -69,7 +115,14 @@ const getData = async():Promise<iData> => {
     const engagementMap = getLocations(embededData)
     const content = getContent(embededData)
     const audience = getNiches(embededData)
+    const suggestions = getSuggestions(embededData)
 
+    const data = { kpis, content, audience, engagementMap, suggestions }
+    console.log(data)
     
-    return { kpis, content, audience, engagementMap }
+    await fs.writeFile('../../app/data/data.json', JSON.stringify(data))
+    return data
 }
+
+
+getData().catch(console.log)
