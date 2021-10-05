4b24cbf8463cfb70806244b7fa483f11c6ca0f1a
Santiago8888 <santiago.marti67@gmail.com>
2021-10-04 21:07:01 -0500

    Email Tweets interface.

--- scripts/V.0.0.5/types/email.ts
+++ scripts/V.0.0.5/types/email.ts
@@ -23,3 +23,33 @@ interface iEmailKpis {
     replies_trend: string
     replies_color: string
 }
+
+
+interface iEmailTweets {
+    best_tweet_text: string
+    best_tweet_impressions: string
+    best_tweet_retweets: string
+    best_tweet_likes: string
+    best_tweet_replies: string
+    best_tweet_profile_visits: string
+    best_tweet_date: string
+    best_tweet_link: string
+
+    best_tweet_text_2: string
+    best_tweet_impressions_2: string
+    best_tweet_retweets_2: string
+    best_tweet_likes_2: string
+    best_tweet_replies_: string
+    best_tweet_profile_visits_2: string
+    best_tweet_date_2: string
+    best_tweet_link_2: string
+
+    best_tweet_text_3: string
+    best_tweet_impressions_3: string
+    best_tweet_retweets_3: string
+    best_tweet_likes_3: string
+    best_tweet_replies_3: string
+    best_tweet_profile_visits_3: string
+    best_tweet_date_3: string
+    best_tweet_link_3: string
+}
