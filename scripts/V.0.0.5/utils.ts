a09b3da6017d25380d99c5983a11b5c5a521070a
Santiago8888 <santiago.marti67@gmail.com>
2021-10-04 22:50:34 -0500

    Build email tweets data.

--- scripts/V.0.0.5/utils.ts
+++ scripts/V.0.0.5/utils.ts
@@ -1,10 +1,13 @@
-import { iEmailKpis, iEmailMetrics } from './types/email'
+import { iEmailKpis, iEmailTweets, iEmailMetrics } from './types/email'
 import { iData } from './types/data'
 
-const formatNumber = (value:number):string => String(value)
 
-export const buildEmailMetrics = ({ kpis }:iData):iEmailMetrics => {
+const formatNumber = (value:number):string => String(value)
+const formatDate = (date:Date):string => String(date)
+
+export const buildEmailMetrics = ({ kpis, bestTweets }:iData):iEmailMetrics => {
     const { followers, engagements, impressions, clicks, tweets, replies } = kpis
+    const { profile, tweets:[tweet1, tweet2, tweet3] } = bestTweets
 
     const emailKpis: iEmailKpis = {
         followers: formatNumber(followers.value),
@@ -30,7 +33,41 @@ export const buildEmailMetrics = ({ kpis }:iData):iEmailMetrics => {
         replies: formatNumber(replies.value),
         replies_trend: formatNumber(replies.trend),
         replies_color: replies.color
-    }    
+    }
+
+    const emailTweets:iEmailTweets = {
+        profile_name: profile.name,
+        profile_handle: profile.handle,
+        profile_link: profile.link,
+        profile_image: profile.image,
+
+        best_tweet_text: tweet1.text,
+        best_tweet_impressions: formatNumber(tweet1.impressions),
+        best_tweet_retweets: formatNumber(tweet1.retweets),
+        best_tweet_likes: formatNumber(tweet1.likes),
+        best_tweet_replies: formatNumber(tweet1.replies),
+        best_tweet_profile_visits: formatNumber(tweet1.profile_visits),
+        best_tweet_date: formatDate(tweet1.date),
+        best_tweet_link: tweet1.link,
+
+        best_tweet_text_2: tweet2.text,
+        best_tweet_impressions_2: formatNumber(tweet2.impressions),
+        best_tweet_retweets_2: formatNumber(tweet2.retweets),
+        best_tweet_likes_2: formatNumber(tweet2.likes),
+        best_tweet_replies_: formatNumber(tweet2.replies),
+        best_tweet_profile_visits_2: formatNumber(tweet2.profile_visits),
+        best_tweet_date_2: formatDate(tweet2.date),
+        best_tweet_link_2: tweet2.link,
+
+        best_tweet_text_3: tweet3.text,
+        best_tweet_impressions_3: formatNumber(tweet3.impressions),
+        best_tweet_retweets_3: formatNumber(tweet3.retweets),
+        best_tweet_likes_3: formatNumber(tweet3.likes),
+        best_tweet_replies_3: formatNumber(tweet3.replies),
+        best_tweet_profile_visits_3: formatNumber(tweet3.profile_visits),
+        best_tweet_date_3: formatDate(tweet3.date),
+        best_tweet_link_3: tweet3.link
+    }
 }
 
 export const writeJSON = () => {}
