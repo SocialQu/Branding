fda5b3a1a450b89354115a52a1384531b6a6832b
Santiago8888 <santiago.marti67@gmail.com>
2021-10-04 22:57:55 -0500

    Build email topics.

--- scripts/V.0.0.5/utils.ts
+++ scripts/V.0.0.5/utils.ts
@@ -1,13 +1,14 @@
-import { iEmailKpis, iEmailTweets, iEmailMetrics } from './types/email'
+import { iEmailKpis, iEmailTweets, iEmailTopics, iEmailMetrics } from './types/email'
 import { iData } from './types/data'
 
 
 const formatNumber = (value:number):string => String(value)
 const formatDate = (date:Date):string => String(date)
 
-export const buildEmailMetrics = ({ kpis, bestTweets }:iData):iEmailMetrics => {
+export const buildEmailMetrics = ({ kpis, bestTweets, topics }:iData):iEmailMetrics => {
     const { followers, engagements, impressions, clicks, tweets, replies } = kpis
     const { profile, tweets:[tweet1, tweet2, tweet3] } = bestTweets
+    const [ topic1, topic2, topic3, topic4, topic5 ] = topics
 
     const emailKpis: iEmailKpis = {
         followers: formatNumber(followers.value),
@@ -67,6 +68,48 @@ export const buildEmailMetrics = ({ kpis, bestTweets }:iData):iEmailMetrics => {
         best_tweet_profile_visits_3: formatNumber(tweet3.profile_visits),
         best_tweet_date_3: formatDate(tweet3.date),
         best_tweet_link_3: tweet3.link
+    }
+
+    const emailTopics: iEmailTopics = {
+        topic_name: topic1.name,
+        topic_color: topic1.color,
+        topic_text: topic1.text,
+        topic_tweets: formatNumber(topic1.tweets),
+        topic_impressions: formatNumber(topic1.impressions),
+        topic_engagements: formatNumber(topic1.engagements),
+        topic_width: formatNumber(topic1.width),
+
+        topic_name_2: topic2.name,
+        topic_color_2: topic2.color,
+        topic_text_2: topic2.text,
+        topic_tweets_2: formatNumber(topic2.tweets),
+        topic_impressions_2: formatNumber(topic2.impressions),
+        topic_engagements_2: formatNumber(topic2.engagements),
+        topic_width_2: formatNumber(topic2.width),
+
+        topic_name_3: topic3.name,
+        topic_color_3: topic3.color,
+        topic_text_3: topic3.text,
+        topic_tweets_3: formatNumber(topic3.tweets),
+        topic_impressions_3: formatNumber(topic3.impressions),
+        topic_engagements_3: formatNumber(topic3.engagements),
+        topic_width_3: formatNumber(topic3.width),
+
+        topic_name_4: topic4.name,
+        topic_color_4: topic4.color,
+        topic_text_4: topic4.text,
+        topic_tweets_4: formatNumber(topic4.tweets),
+        topic_impressions_4: formatNumber(topic4.impressions),
+        topic_engagements_4: formatNumber(topic4.engagements),
+        topic_width_4: formatNumber(topic4.width),
+
+        topic_name_5: topic5.name,
+        topic_color_5: topic5.color,
+        topic_text_5: topic5.text,
+        topic_tweets_5: formatNumber(topic5.tweets),
+        topic_impressions_5: formatNumber(topic5.impressions),
+        topic_engagements_5: formatNumber(topic5.engagements),
+        topic_width_5: formatNumber(topic5.width),
     }
 }
 
