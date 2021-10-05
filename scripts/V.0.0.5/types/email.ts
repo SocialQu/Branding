5a56ad35b6676dc847964e802bf094e4d44b5f4e
Santiago8888 <santiago.marti67@gmail.com>
2021-10-04 21:06:11 -0500

    Email KPIs.

---
+++ scripts/V.0.0.5/types/email.ts
@@ -0,0 +1,25 @@
+interface iEmailKpis {
+    followers: string
+    new_followers: string
+    follower_color: string
+
+    engagements: string
+    engagements_trend: string
+    engagements_color: string
+
+    impressions: string
+    impressions_trend: string
+    impressions_color: string
+
+    clicks: string
+    clicks_trend: string
+    clicks_color: string
+    
+    tweets: string
+    tweets_trend: string
+    tweets_color: string
+    
+    replies: string
+    replies_trend: string
+    replies_color: string
+}
