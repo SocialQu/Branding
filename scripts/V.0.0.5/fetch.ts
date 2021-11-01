ff4abff50b89e617b6405625befbf6c6488b4482
Santiago8888 <santiago.marti67@gmail.com>
2021-11-01 13:58:38 -0600

    Return early on no mentions.

--- scripts/V.0.0.5/fetch.ts
+++ scripts/V.0.0.5/fetch.ts
@@ -144,6 +144,8 @@ export const fetchData = async({ access_token_key, access_token_secret }:iSubscr
 
 interface iProfileImages { [username:string]:string }
 export const fetchMentions = async(clients:iTwitterClients, mentions:string[]):Promise<iProfileImages> => {
+    if(!mentions.length) return {}
+
     const { metricsClient } = getTwitterClients(clients)
 
     const url = `users/by?usernames=${mentions.join(',')}&user.fields=profile_image_url`
