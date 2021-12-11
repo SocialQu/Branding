8ff8b218e12942073b6a6a230cfb0faf3e84c486
Santiago8888 <santiago.marti67@gmail.com>
2021-12-11 13:13:08 -0600

    Write tweets & metrics.

--- scripts/V.0.1.1/fetch.ts
+++ scripts/V.0.1.1/fetch.ts
@@ -22,11 +22,14 @@ interface iUser {
     access_token_secret:string
 }
 
-const fetchTweets = async({ screen_name, access_token_key, access_token_secret }: iUser) => {
+const fetchUser = async({ screen_name, access_token_key, access_token_secret }: iUser) => {
     const { client, metricsClient } = getTwitterClients({ access_token_key, access_token_secret })
-    const rawTweets:iRawTweet[] = await client.get(`/statuses/user_timeline.json?count=100`) 
 
-    const noRetweets = rawTweets.filter(({ retweeted }) => !retweeted)
+    const tweets:iRawTweet[] = await client.get(`/statuses/user_timeline.json?count=100`) 
+    const tweetsData = JSON.stringify(tweets)
+    await fs.writeFile(`./data/tweets/${screen_name}.json`, tweetsData)
+
+    const noRetweets = tweets.filter(({ retweeted }) => !retweeted)
     if(!noRetweets.length) return []
 
     const ids:string = noRetweets.map(({ id_str }) => id_str).join(',')
@@ -35,41 +38,8 @@ const fetchTweets = async({ screen_name, access_token_key, access_token_secret }
     const metricsUrl = `tweets?ids=${ids}&tweet.${fields}`
 
     const { data:metrics }:{ data: iRawMetrics[]} = await metricsClient.get(metricsUrl)
-    if (!metrics) return []
-
-    const tweetsWithMetrics = noRetweets.map((t) => ({
-        ...t,
-        metrics: metrics.find(({ id }) => t.id_str === id) as iRawMetrics
-    }))
-
-    const tweets:iTweet[] = rawTweets.map(t => {
-        const tweet:iTweet = {
-            id: t.id_str,
-            text: t.text,
-            isReply: !!t.in_reply_to_status_id,
-            datetime: t.created_at
-        }
-
-        const tweetWithMetrics = tweetsWithMetrics.find(({ id_str }) => id_str === t.id_str)
-
-        if(tweetWithMetrics?.metrics){
-            const { metrics } = tweetWithMetrics
-
-            tweet.metrics = {
-                likes: metrics.organic_metrics.like_count,
-                replies: metrics.organic_metrics.reply_count,
-                retweets: metrics.organic_metrics.retweet_count,
-                impressions: metrics.organic_metrics.impression_count,
-                visits: metrics.organic_metrics.user_profile_clicks,
-                clicks: metrics.organic_metrics.url_link_clicks || 0
-            }
-        }
-
-        return tweet
-
-    })
-
-    return tweets
+    const metricsData = JSON.stringify(metrics)
+    await fs.writeFile(`./data/metrics/${screen_name}.json`, metricsData)
 }
 
 
@@ -104,14 +74,8 @@ const fetch = async(users:iUser[], idx:number) => {
 
     const user = users[idx]
 
-    try {
-        const tweets = await fetchTweets(user)
-        const tweetsData = JSON.stringify(tweets)
-    
-        await fs.writeFile(`./data/tweets/${user.screen_name}.json`, tweetsData)
-        console.log(`Fetched ${tweets.length} tweets from ${user.screen_name}.`)
-
-    } catch(e) { console.log(`Error fetching ${user.screen_name} tweets: ${e}`) }
+    try { const tweets = await fetchUser(user) } 
+    catch(e) { console.log(`Error fetching ${user.screen_name} tweets: ${e}`) }
 
     fetch(users, idx + 1)
 }
