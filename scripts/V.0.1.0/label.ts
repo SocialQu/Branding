edc25dd82996a49fb3152d66208b2bc51cc6a304
Santiago8888 <santiago.marti67@gmail.com>
2021-12-12 12:19:26 -0600

    Set types on label function.

--- scripts/V.0.1.0/label.ts
+++ scripts/V.0.1.0/label.ts
@@ -1,4 +1,53 @@
 import { iAggregatedUser } from './types/aggregated'
 import { iLabeledData } from './types/labeled'
 
-const label = () => {}
+const label = (data:iAggregatedUser):iLabeledData => {
+    return {
+        followers: 0,
+        following: 0,
+
+        isReply: 0,
+
+        emojis: 0,
+        hasEmojis: 0,
+
+        characterLength: 0,
+        wordLength: 0,
+        sentenceLength: 0,
+
+        lineBreaks: 0,
+        hasLineBreaks: 0,
+
+        links: 0,
+        hasLinks: 0,
+
+        hasMedia: 0,
+        media: 0,
+
+        hasHashtags: 0,
+        hashtags: 0,
+
+        hasMentions: 0,
+        mentions: 0,
+
+        isDaytime: 0,
+        isWeekDay: 0,
+
+        hoursFromLastStatus: 0,
+        hoursFromLastTweet: 0,
+
+        lastTweetLikes: 0,
+        lastTweetReplies:0,
+        lastTweetClicks: 0,
+        lastTweetVisits: 0,
+        lastTweetRetweets: 0,
+        lastTweetImpressions: 0,
+
+        likes:0,
+        clicks: 0,
+        visits: 0,
+        replies: 0,
+        retweets: 0,
+        impressions: 0
+    }
+}
