5a715b6926ba3ca03b9beabaf513402ae0ab7a3d
Santiago8888 <santiago.marti67@gmail.com>
2021-12-12 12:22:54 -0600

    Return array on label, rather than single object.

--- scripts/V.0.1.0/label.ts
+++ scripts/V.0.1.0/label.ts
@@ -1,53 +1,52 @@
 import { iAggregatedUser } from './types/aggregated'
 import { iLabeledData } from './types/labeled'
 
-const label = (data:iAggregatedUser):iLabeledData => {
-    return {
-        followers: data.followers,
-        following: data.following,
 
-        isReply: 0,
+const label = ({ tweets, ...data }: iAggregatedUser):iLabeledData[] => tweets.map(() => ({
+    followers: data.followers,
+    following: data.following,
 
-        emojis: 0,
-        hasEmojis: 0,
+    isReply: 0,
 
-        characterLength: 0,
-        wordLength: 0,
-        sentenceLength: 0,
+    emojis: 0,
+    hasEmojis: 0,
 
-        lineBreaks: 0,
-        hasLineBreaks: 0,
+    characterLength: 0,
+    wordLength: 0,
+    sentenceLength: 0,
 
-        links: 0,
-        hasLinks: 0,
+    lineBreaks: 0,
+    hasLineBreaks: 0,
 
-        hasMedia: 0,
-        media: 0,
+    links: 0,
+    hasLinks: 0,
 
-        hasHashtags: 0,
-        hashtags: 0,
+    hasMedia: 0,
+    media: 0,
 
-        hasMentions: 0,
-        mentions: 0,
+    hasHashtags: 0,
+    hashtags: 0,
 
-        isDaytime: 0,
-        isWeekDay: 0,
+    hasMentions: 0,
+    mentions: 0,
 
-        hoursFromLastStatus: 0,
-        hoursFromLastTweet: 0,
+    isDaytime: 0,
+    isWeekDay: 0,
 
-        lastTweetLikes: 0,
-        lastTweetReplies:0,
-        lastTweetClicks: 0,
-        lastTweetVisits: 0,
-        lastTweetRetweets: 0,
-        lastTweetImpressions: 0,
+    hoursFromLastStatus: 0,
+    hoursFromLastTweet: 0,
 
-        likes:0,
-        clicks: 0,
-        visits: 0,
-        replies: 0,
-        retweets: 0,
-        impressions: 0
-    }
-}
+    lastTweetLikes: 0,
+    lastTweetReplies:0,
+    lastTweetClicks: 0,
+    lastTweetVisits: 0,
+    lastTweetRetweets: 0,
+    lastTweetImpressions: 0,
+
+    likes:0,
+    clicks: 0,
+    visits: 0,
+    replies: 0,
+    retweets: 0,
+    impressions: 0
+}))
