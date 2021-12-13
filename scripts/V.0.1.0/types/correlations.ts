ab2a2a94ed897f9a7a440117bc2592e72ad5aabe
Santiago8888 <santiago.marti67@gmail.com>
2021-12-13 10:59:47 -0600

    Declare correlation outputs.

--- scripts/V.0.1.0/types/correlations.ts
+++ scripts/V.0.1.0/types/correlations.ts
@@ -1,6 +1,14 @@
 import { iMetrics } from './aggregated'
 
-interface iOutputs extends iMetrics {}
 
-interface iOutputCorrelations {}
+interface iOutputs extends iMetrics {} // Stores the correlation coefficient [-1, 1] for each output.
+
+interface iOutputCorrelations {
+    likes: iOutputs
+    clicks: iOutputs
+    visits: iOutputs 
+    replies: iOutputs
+    retweets: iOutputs
+    impressions: iOutputs
+}
 
