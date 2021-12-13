dfff6aa3a63c4e6fe8627cd92060e41d80910bc1
Santiago8888 <santiago.marti67@gmail.com>
2021-12-12 19:05:58 -0600

    Create a tweet without dependance on last status.

--- scripts/V.0.1.0/label.ts
+++ scripts/V.0.1.0/label.ts
@@ -5,13 +5,46 @@ import { Duple, iLabeledData } from './types/labeled'
 const toDuple = (dual:boolean):Duple => dual ? 1 : 0
 const getLastTweet = (tweets:iAggregatedTweet[], i:number) => tweets.find((t, idx) => !t.isReply && idx > i)
 
-const label = ({ tweets, ...data }: iAggregatedUser):iLabeledData[] => tweets.filter((_, i) => 
-    getLastTweet(tweets, i)
-).map((t, i) => {
+const label = ({ tweets, ...data }: iAggregatedUser):iLabeledData[] => tweets.map((t, i) => {
 
     const date = new Date(t.datetime)
     const hours = date.getHours()
     const day = date.getDay()
+
+    const tweet:iLabeledData = {
+        followers: data.followers,
+        following: data.following,
+
+        isReply: toDuple(t.isReply),
+
+        emojis: 0,
+        hasEmojis: 0 as Duple,
+
+        characterLength: t.text.length,
+        wordLength: t.text.split(/[\s]+/).length,
+        sentenceLength: t.text.split('.').length,
+        pargagraphLength: t.text.split('\n').length,
+
+        lineBreaks: (t.text.match(/\n/g) || []).length,
+        hasLineBreaks: toDuple(!!(t.text.match(/\n/g) || []).length),
+
+        links: t.entities.links.length,
+        hasLinks: toDuple(!!t.entities.links.length),
+
+        media: t.entities.media.length,
+        hasMedia: toDuple(!!t.entities.media.length),
+
+        hashtags: t.entities.hashtags.length,
+        hasHashtags: toDuple(!!t.entities.hashtags.length),
+
+        mentions: t.entities.mentions.length,
+        hasMentions: toDuple(!!t.entities.mentions.length),
+
+        isDaytime: toDuple(hours > 9 && hours < 21),
+        isWeekDay: toDuple(day === 0 || day === 6),
+
+        ...t.metrics
+    }
 
     const lastStatus = tweets[i + 1]
     const lastTweet = getLastTweet(tweets, i) as iAggregatedTweet
