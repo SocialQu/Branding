0a1ff5df60d923db67507aab2b60e0d22f5f6dc4
Santiago8888 <santiago.marti67@gmail.com>
2021-12-14 06:01:41 -0600

    Define empty correlations object.

---
+++ scripts/V.0.1.0/correlate.ts
@@ -0,0 +1,15 @@
+import { iCorrelations } from './types/correlations'
+import { iLabeledTweet } from './types/labeled'
+
+
+const correlations:iCorrelations = {
+    likes:0,
+    replies:0,
+    retweets:0,
+    visits:0,
+    clicks:0,
+    impressions:0,
+    engagements:0
+}
+
+const outputCorrelations = (tweets:iLabeledTweet[]) => {}
