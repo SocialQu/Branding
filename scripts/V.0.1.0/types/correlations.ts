d428a9ed1bf09cd5340fc80fdaeccdecec29f187
Santiago8888 <santiago.marti67@gmail.com>
2021-12-14 05:41:32 -0600

    Input correlations matrix.

--- scripts/V.0.1.0/types/correlations.ts
+++ scripts/V.0.1.0/types/correlations.ts
@@ -11,4 +11,57 @@ interface iOutputCorrelations {
     impressions: iCorrelations
 }
 
-interface iInputCorrelations {}
+interface iInputCorrelations {
+    // Bio
+    followers: iCorrelations
+    following: iCorrelations
+
+    // Reply
+    isReply: iCorrelations
+
+    // Emojis
+    hasEmojis: iCorrelations
+    emojis: iCorrelations
+
+    // Length
+    characterLength: iCorrelations
+    wordLength: iCorrelations
+    sentenceLength: iCorrelations
+    pargagraphLength: iCorrelations
+
+    // Line breaks
+    hasLineBreaks: iCorrelations
+    lineBreaks: iCorrelations
+
+    // Media
+    hasMedia: iCorrelations
+    media: iCorrelations
+
+    // Links
+    hasLinks: iCorrelations
+    links: iCorrelations
+
+    // Hashtags
+    hasHashtags: iCorrelations
+    hashtags: iCorrelations
+
+    // Mentions
+    hasMentions: iCorrelations
+    mentions: iCorrelations
+
+    // Datetime
+    isWeekDay: iCorrelations
+    isDaytime: iCorrelations
+
+    // Last tweet
+    hoursFromLastTweet: iCorrelations
+    hoursFromLastStatus: iCorrelations
+
+    // Recent metrics
+    lastTweetLikes: iCorrelations
+    lastTweetVisits: iCorrelations
+    lastTweetClicks: iCorrelations
+    lastTweetReplies: iCorrelations
+    lastTweetRetweets: iCorrelations
+    lastTweetImpressions: iCorrelations
+}
