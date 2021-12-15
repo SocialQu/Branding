8406b5f902970afc1f01c10880fc38511475855e
Santiago8888 <santiago.marti67@gmail.com>
2021-12-14 21:23:19 -0600

    Import outputs for correlation interfaces.

--- scripts/V.0.1.0/types/correlations.ts
+++ scripts/V.0.1.0/types/correlations.ts
@@ -1,6 +1,5 @@
-import { iMetrics  } from './aggregated'
+import { iOutputs as iCorrelations } from './labeled'
 
-export interface iCorrelations extends iMetrics { engagements:number }
 
 // Stores the correlation coefficient [-1, 1] for each output in a matrix.
 export interface iOutputCorrelations {
