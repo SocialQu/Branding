3ce8ed0453877e6fa734e2c0b600609179bb2e39
Santiago8888 <santiago.marti67@gmail.com>
2021-12-19 08:17:39 -0600

    Add extra info on dimensions analysis.

--- scripts/V.0.1.0/embed.ts
+++ scripts/V.0.1.0/embed.ts
@@ -1,6 +1,7 @@
 import MLR from 'ml-regression-multivariate-linear'
 import { iReducedTweet } from './types/embeddings'
 import tweets from './data/reducedTweets.json'
+import { iOutputs } from './types/labeled'
 import { writeFile } from 'fs/promises'
 import { labels } from './regression'
 
@@ -22,17 +23,23 @@ const linearRegression = (tweets:iReducedTweet[]):number[][] => {
 }
 
 interface iFindTweets { tweets:iReducedTweet[], weights:number[][] }
-interface iTopTweets { dimension:number, weight:number[], texts:string[] } // Top tweets by dimension
+interface iWeight { label: keyof iOutputs, value: number }
+interface iTopTweet { text:string, engagements:number, value:number }
+interface iTopTweets { dimension:number, weight:iWeight[], topTweets:iTopTweet[] } // Top tweets by dimension
 const findTweets = ({ tweets, weights }:iFindTweets):iTopTweets[] => tweets[0].reduced.map(( _, i) => i).map((i) => {
-    const sorted = tweets.sort(({ reduced:a }, { reduced:b }) => a[i] > b[i] ? 1 : -1)
-    const texts = sorted.filter((_, i) => i < 10).map(({ text }) => text)
-    return { dimension:i, weight: weights[i], texts }
+    const sorted = tweets.sort(({ reduced:a }, { reduced:b }) => a[i] > b[i] ? -1 : 1)
+    const topTweets = sorted.filter((_, i) => i < 10).map(({ text, engagements, reduced }) => ({
+        text, engagements, value:reduced[i] 
+    }))
+
+    const weight = labels.map((label, idx) => ({label, value:weights[i][idx]}))
+    return { dimension:i, weight, topTweets }
 })
 
 const analyzeEmbeddings = (tweets:iReducedTweet[]) => {
     const weights = linearRegression(tweets)
     const results = findTweets({ tweets, weights })
-    console.log('results', results)
+    results.map(r => console.log(r))
 }
 
 analyzeEmbeddings(tweets as iReducedTweet[])
