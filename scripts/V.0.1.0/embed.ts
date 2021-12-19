21e826654fe5541a2eac14a89d7670b30bf3312e
Santiago8888 <santiago.marti67@gmail.com>
2021-12-18 22:09:51 -0600

    Preliminary embed tweets process.

--- scripts/V.0.1.0/embed.ts
+++ scripts/V.0.1.0/embed.ts
@@ -2,4 +2,11 @@ import { UniversalSentenceEncoder as iModel } from '@tensorflow-models/universal
 import { iLabeledTweet } from './types/labeled'
 
 const embedTweets = async(tweets:iLabeledTweet[], model:iModel) => {
+    const texts = tweets.map(({ text }) => text)
+
+    const tensors = await model.embed(texts)
+    const embeddings = tensors.arraySync()
+
+    const embeddedTweets = tweets.map((t,i) => ({...t, embeddings:embeddings[i] }))
+    return embeddedTweets
 }
