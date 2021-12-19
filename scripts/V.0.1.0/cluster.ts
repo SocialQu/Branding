588b8fdbd384157ab5e6b8bbb0cdde2dfd4bd217
Santiago8888 <santiago.marti67@gmail.com>
2021-12-19 15:58:13 -0600

    Get Zscore params.

--- scripts/V.0.1.0/cluster.ts
+++ scripts/V.0.1.0/cluster.ts
@@ -1,9 +1,17 @@
+import { zScore, mean, standardDeviation } from 'simple-statistics'
 import { kMeansCluster } from 'simple-statistics'
 import tweets from './data/reducedTweets.json'
-
+import { iLabeledTweet } from './types/labeled'
 
 const getEmbeddingsClusters = () => {
     const embeddings = tweets.map(({ reduced }) => reduced)
     const { labels, centroids } = kMeansCluster(embeddings, 9)
     return labels    
 }
+
+const getZScoreParams = (tweets:iLabeledTweet[], metric:keyof iLabeledTweet) => {
+    const values = tweets.map(t => t[metric] as number)
+    return { mean:mean(values), sd:standardDeviation(values) }
+}
+
+const getEngagementClusters = () => {}
