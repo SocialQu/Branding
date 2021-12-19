b61faab175243e2142610d8ae2dce6aedcaa734c
Santiago8888 <santiago.marti67@gmail.com>
2021-12-18 22:25:27 -0600

    Export reduced interface.

--- scripts/V.0.1.0/types/embeddings.ts
+++ scripts/V.0.1.0/types/embeddings.ts
@@ -1,8 +1,8 @@
 import { iLabeledTweet } from './labeled'
 
-export interface iEmbeddedTweet extends iLabeledTweet { embeddings:number[] }
 
-interface iReducedTweet extends iEmbeddedTweet { reduced:number[] }
+export interface iEmbeddedTweet extends iLabeledTweet { embeddings:number[] }
+export interface iReducedTweet extends iEmbeddedTweet { reduced:number[] }
 
 interface iClusteredTweet extends iReducedTweet {
     embeddingsCluster: number
