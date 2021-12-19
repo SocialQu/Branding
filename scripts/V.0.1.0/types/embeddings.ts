b790f7e69f903184ca0ab17f8c00365f6933ba47
Santiago8888 <santiago.marti67@gmail.com>
2021-12-19 15:47:42 -0600

    Export clustered tweet.

--- scripts/V.0.1.0/types/embeddings.ts
+++ scripts/V.0.1.0/types/embeddings.ts
@@ -4,7 +4,7 @@ import { iLabeledTweet } from './labeled'
 export interface iEmbeddedTweet extends iLabeledTweet { embeddings:number[] }
 export interface iReducedTweet extends iLabeledTweet { reduced:number[] }
 
-interface iClusteredTweet extends iReducedTweet {
+export interface iClusteredTweet extends iReducedTweet {
     embeddingsCluster: number
     engagementsCluster: number
     featuresCluster: number
