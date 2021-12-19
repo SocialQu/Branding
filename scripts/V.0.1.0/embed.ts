3bad63d2ec4785ab38a5a7decff23392b0e862df
Santiago8888 <santiago.marti67@gmail.com>
2021-12-18 23:02:58 -0600

    Map results from weights.

--- scripts/V.0.1.0/embed.ts
+++ scripts/V.0.1.0/embed.ts
@@ -8,4 +8,8 @@ const linearRegression = (tweets:iReducedTweet[]) => {
     const y = tweets.map((t) => labels.map((f) => t[f]))
 
     const { weights } = new MLR(x, y)
+    const results = labels.reduce((d, k, i) => ({ 
+        ...d, 
+        [k]: tweets[i].reduced.reduce((d, _, idx) => ({...d, [idx]: weights[idx][i] }), {}) }), {}
+    )
 }
