4d1e83c952341744665f146c8d297cc1d7638b2b
Santiago8888 <santiago.marti67@gmail.com>
2021-12-18 22:08:49 -0600

    Define embed tweet method.

---
+++ scripts/V.0.1.0/embed.ts
@@ -0,0 +1,5 @@
+import { UniversalSentenceEncoder as iModel } from '@tensorflow-models/universal-sentence-encoder'
+import { iLabeledTweet } from './types/labeled'
+
+const embedTweets = async(tweets:iLabeledTweet[], model:iModel) => {
+}
