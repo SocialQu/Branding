2dd5c38949b02be300d894913e19b771c746e845
Santiago8888 <santiago.marti67@gmail.com>
2021-12-21 19:09:47 -0600

    Fetch & write topics with colors.

--- scripts/V.0.1.0/classification.ts
+++ scripts/V.0.1.0/classification.ts
@@ -1,1 +1,33 @@
-import topics from '../V.0.0.5/data/colors.json'
+import colors from '../V.0.0.5/data/colors.json'
+import tweets from './data/clusteredTweets.json'
+import { writeFile } from 'fs/promises'
+import { MongoClient } from 'mongodb'
+
+
+interface iTopic {
+    _id: string
+    topic: string
+    color?: string
+    embeddings: number[]
+    center: number[]
+}
+
+export const fetchTopics = async():Promise<iTopic[]> => {
+    const uri = `mongodb+srv://${process.env.mongo_admin}/${process.env.cortazar_db}`
+
+    const client = new MongoClient(uri)
+    await client.connect()
+
+    const Topics = client.db('Cortazar').collection('topics')
+    const topics:iTopic[] = await Topics.find().toArray()
+
+    await client.close()
+
+    const findColor = ({topic}:iTopic) => colors.find(({ topic:t }) => topic === t)?.color || ''
+    const coloredTopics = topics.map(topic => ({...topic, color:findColor(topic) })) as iTopic[]
+
+    const colorsData = JSON.stringify(coloredTopics.map(({ color, topic }) => ({ color, topic })))
+    await writeFile('./data/topics.json', colorsData)
+
+    return coloredTopics
+}
