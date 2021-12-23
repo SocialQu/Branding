3cce804f28578a08acfcadb47090dc3fa60f8869
Santiago8888 <santiago.marti67@gmail.com>
2021-12-23 11:28:59 -0600

    Word embedding CSV columns.

--- scripts/V.0.1.0/normalization.ts
+++ scripts/V.0.1.0/normalization.ts
@@ -4,6 +4,7 @@ import { ObjectHeaderItem } from 'csv-writer/src/lib/record'
 
 const path = './data/tweets.csv'
 const header:ObjectHeaderItem[] = [
+    // Features
     { id:'day', title:'DAY' },
     { id:'hour', title:'HOUR' },
     { id:'media', title:'MEDIA' },
@@ -14,7 +15,21 @@ const header:ObjectHeaderItem[] = [
     { id:'mentions', title:'MENTIONS' },
     { id:'followers', title:'FOLLOWERS' },
     { id:'following', title:'FOLLOWING' },
-    { id:'linebreaks', title:'LINEBREAKS' }
+    { id:'linebreaks', title:'LINEBREAKS' },
+
+    // Word Embeddings
+    { id:'we1', title:'WE_1' },
+    { id:'we2', title:'WE_2' },
+    { id:'we3', title:'WE_3' },
+    { id:'we4', title:'WE_4' },
+    { id:'we5', title:'WE_5' },
+    { id:'we6', title:'WE_6' },
+    { id:'we7', title:'WE_7' },
+    { id:'we8', title:'WE_8' },
+    { id:'we9', title:'WE_9' },
+    { id:'we10', title:'WE_10' },
+    { id:'we11', title:'WE_11' },
+    { id:'we12', title:'WE_12' },
 ]
 
 
