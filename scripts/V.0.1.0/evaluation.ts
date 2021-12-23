49c83ac275768bdcdab06af7576448399fd95352
Santiago8888 <santiago.marti67@gmail.com>
2021-12-22 19:39:17 -0600

    Predict values of linear regression.

---
+++ scripts/V.0.1.0/evaluation.ts
@@ -0,0 +1,23 @@
+import MLR from 'ml-regression-multivariate-linear'
+import { iInputs, iOutputs } from './types/labeled'
+import tweets from './data/labeledData.json'
+import { writeFile } from 'fs/promises'
+
+const features:(keyof iInputs)[] = [
+    'followers', 'following', // Bio
+    'isDaytime', 'isWeekDay', // Datetime
+    'emojis', 'isReply', // Structure
+    'characterLength', 'pargagraphLength', // Length
+    'hasHashtags', 'mentions', 'hasMedia', 'hasLinks', // Entities
+]
+
+export const labels:(keyof iOutputs)[] = [ 
+    'engagements', 'impressions', 'likes', 'retweets', 'replies', 'clicks', 'visits'
+]
+
+const X = tweets.map((t) => features.map((f) => t[f] as number))
+const Y = tweets.map((t) => labels.map((f) => t[f]))
+
+const mlr = new MLR(X, Y)
+
+const predictions = X.map((x, i) => mlr.predict(x))
