f17fd1fe8efe1e73db52f0574df58ef173315be9
Santiago8888 <santiago.marti67@gmail.com>
2021-12-22 19:43:06 -0600

    Quadratic error function.

--- scripts/V.0.1.0/evaluation.ts
+++ scripts/V.0.1.0/evaluation.ts
@@ -23,3 +23,4 @@ const mlr = new MLR(X, Y)
 const predictions = X.map((x, i) => mlr.predict(x))
 
 const linearError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += Math.abs(x[0] - Y[i][0]), 0)
+const quadraticError = (X:number[][], Y:number[][]) => Math.sqrt(X.reduce((d, x, i) => d += (x[0] - Y[i][0])**2, 0))
