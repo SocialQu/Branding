58e9acfe66c29c683c0b57accb6fbdceeb6d9813
Santiago8888 <santiago.marti67@gmail.com>
2021-12-22 19:58:05 -0600

    Add min to log delta computation.

--- scripts/V.0.1.0/evaluation.ts
+++ scripts/V.0.1.0/evaluation.ts
@@ -26,7 +26,7 @@ const predictions = X.map((x, i) => mlr.predict(x))
 const linearError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += Math.abs(x[0] - Y[i][0]), 0)
 const quadraticError = (X:number[][], Y:number[][]) => Math.sqrt(X.reduce((d, x, i) => d += (x[0] - Y[i][0])**2, 0))
 
-const logDelta = (x:number, y:number) => Math.abs(Math.log(x+1) - Math.log(y+1))
+const logDelta = (x:number, y:number) => Math.abs(Math.log(x+1) - Math.log(Math.max(y, 1)))
 const logError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += logDelta(x[0], Y[i][0]), 0)
 
 
