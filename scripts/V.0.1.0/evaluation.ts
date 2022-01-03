17d31abab12a337e0a620d96c3b6b37e7cbdbc4d
Santiago8888 <santiago.marti67@gmail.com>
2022-01-02 21:18:12 -0600

    Write predictions.

--- scripts/V.0.1.0/evaluation.ts
+++ scripts/V.0.1.0/evaluation.ts
@@ -3,6 +3,11 @@ import { iInputs, iOutputs } from './types/labeled'
 import tweets from './data/labeledData.json'
 import { writeFile } from 'fs/promises'
 
+
+export const labels:(keyof iOutputs)[] = [ 
+    'engagements', 'impressions', 'likes', 'retweets', 'replies', 'clicks', 'visits'
+]
+
 const features:(keyof iInputs)[] = [
     'followers', 'following', // Bio
     'isDaytime', 'isWeekDay', // Datetime
@@ -11,29 +16,38 @@ const features:(keyof iInputs)[] = [
     'hasHashtags', 'mentions', 'hasMedia', 'hasLinks', // Entities
 ]
 
-export const labels:(keyof iOutputs)[] = [ 
-    'engagements', 'impressions', 'likes', 'retweets', 'replies', 'clicks', 'visits'
-]
 
-const X = tweets.map((t) => features.map((f) => t[f] as number))
-const Y = tweets.map((t) => labels.map((f) => t[f]))
+const writeRegressionPredictions = () => {
+    const X = tweets.map((t) => features.map((f) => t[f] as number))
+    const Y = tweets.map((t) => labels.map((f) => t[f]))
+    
+    const mlr = new MLR(X, Y)
+    
+    const predictions = X.map((x, i) => mlr.predict(x))
 
-const mlr = new MLR(X, Y)
-
-const predictions = X.map((x, i) => mlr.predict(x))
-
-
-const linearError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += Math.abs(x[0] - Y[i][0]), 0)
-const quadraticError = (X:number[][], Y:number[][]) => Math.sqrt(X.reduce((d, x, i) => d += (x[0] - Y[i][0])**2, 0))
-
-const logDelta = (x:number, y:number) => Math.abs(Math.log(x+1) - Math.log(Math.max(y, 1)))
-const logError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += logDelta(x[0], Y[i][0]), 0)
-
-
-const linearRegressionError = {
-    linear: linearError(Y, predictions)/X.length,
-    quadratic: quadraticError(Y, predictions)/X.length,
-    logarithmic: logError(Y, predictions)/X.length
+    writeFile('data/predictions_regression.json', JSON.stringify(predictions.map(([x]) => x)))
+    return predictions
 }
 
-console.log('Linear Regression Error:', linearRegressionError)
+
+const printPredctions = (predictions:number[][]) => {
+    const Y = tweets.map((t) => labels.map((f) => t[f]))
+
+    const linearError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += Math.abs(x[0] - Y[i][0]), 0)
+    const quadraticError = (X:number[][], Y:number[][]) => Math.sqrt(X.reduce((d, x, i) => d += (x[0] - Y[i][0])**2, 0))
+    
+    const logDelta = (x:number, y:number) => Math.abs(Math.log(x+1) - Math.log(Math.max(y, 1)))
+    const logError = (X:number[][], Y:number[][]) => X.reduce((d, x, i) => d += logDelta(x[0], Y[i][0]), 0)
+    
+    
+    const linearRegressionError = {
+        linear: linearError(Y, predictions)/Y.length,
+        quadratic: quadraticError(Y, predictions)/Y.length,
+        logarithmic: logError(Y, predictions)/Y.length
+    }    
+
+    console.log('Linear Regression Error:', linearRegressionError)
+}
+
+const predictions = writeRegressionPredictions()
+printPredctions(predictions)
